0ecffbf73512042c2e2683c0b65224452d12bb78
hyunwoo0318 <hyunwoo0318@naver.com>
2023-10-26 00:30:38 +0900

    search 결과 정렬 로직 추가

--- src/main/java/Project/OpenBook/Domain/Search/SearchService.java
+++ src/main/java/Project/OpenBook/Domain/Search/SearchService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,15 +29,18 @@ public class SearchService {
 
         List<ChapterSearchResultDto> chapterList = chapterSearchList.stream()
                 .map(cs -> new ChapterSearchResultDto(cs.getChapterNumber(), cs.getTitle()))
+                .sorted(Comparator.comparing(ChapterSearchResultDto::getChapterNumber))
                 .collect(Collectors.toList());
 
         List<TopicSearchResultDto> topicList = topicSearchList.stream()
                 .map(ts -> new TopicSearchResultDto(ts.getChapterNumber(), ts.getChapterTitle(), ts.getTitle()))
+                .sorted(Comparator.comparing(TopicSearchResultDto::getChapterNumber))
                 .collect(Collectors.toList());
 
         List<KeywordSearchResultDto> keywordList = keywordSearchList.stream()
                 .map(ks -> new KeywordSearchResultDto(ks.getChapterNumber(), ks.getChapterTitle(), ks.getTopicTitle(),
                         ks.getName(), ks.getComment()))
+                .sorted(Comparator.comparing(KeywordSearchResultDto::getChapterNumber))
                 .collect(Collectors.toList());
 
         return new SearchResultDto(chapterList, topicList, keywordList);
